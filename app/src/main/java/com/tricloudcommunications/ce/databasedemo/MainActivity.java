c5f53f9ca772cad46cd793023194d541243c5ff3
“Ori <chandleretienne@gmail.com>
2016-12-16 14:21:46 -0600

    Initial commit

---
+++ app/src/main/java/com/tricloudcommunications/ce/databasedemo/MainActivity.java
@@ -0,0 +1,77 @@
+package com.tricloudcommunications.ce.databasedemo;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        try {
+
+            SQLiteDatabase myDatabase = this.openOrCreateDatabase("Users", MODE_PRIVATE, null);
+            //myDatabase.execSQL("CREATE TABLE IF NOT EXISTS users (name VARCHAR, age INT(3))");
+            //myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Chandler', 37)");
+            //myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Laila', 8)");
+
+            Cursor c = myDatabase.rawQuery("SELECT * FROM users", null);
+
+            int nameIndex = c.getColumnIndex("name");
+            int ageIndex = c.getColumnIndex("age");
+
+            c.moveToFirst();
+
+            while (c != null){
+
+                Log.i("name", c.getString(nameIndex));
+                Log.i("age", Integer.toString(c.getInt(ageIndex)));
+
+                c.moveToNext();
+
+            }
+
+        }catch (Exception e){
+
+            e.printStackTrace();
+
+        }
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
